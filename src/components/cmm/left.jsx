b21134dc8b96083106547338ebba673a193b324f
kmee <devKmee@gmail.com>
2026-08-11 09:24:43 +0900

    CSS 폰트색상변경2

--- src/components/cmm/left.jsx
+++ src/components/cmm/left.jsx
@@ -32,16 +32,16 @@ export default function LEFT() {
                 }}
             >
                 <ul style={{ textAlign: 'left', padding: '20px' }}>
-                    <ol id="0" onClick={() => handleClick(0)} style={{ color: page === '0' ? '#280956' : 'gray' }}>
+                    <ol id="0" onClick={() => handleClick(0)} className={page === '0' ? 'active' : ''}>
                         1. Intro
                     </ol>
-                    <ol id="1" onClick={() => handleClick(1)} style={{ color: page === '1' ? '#280956' : 'gray' }}>
+                    <ol id="1" onClick={() => handleClick(1)} className={page === '1' ? 'active' : ''}>
                         2. {cha2.nameKor}
                     </ol>
-                    <ol id="2" onClick={() => handleClick(2)} style={{ color: page === '2' ? '#280956' : 'gray' }}>
+                    <ol id="2" onClick={() => handleClick(2)} className={page === '2' ? 'active' : ''}>
                         3. {cha1.nameKor}
                     </ol>
-                    <ol id="3" onClick={() => handleClick(3)} style={{ color: page === '3' ? '#280956' : 'gray' }}>
+                    <ol id="3" onClick={() => handleClick(3)} className={page === '3' ? 'active' : ''}>
                         4. 관계 및 서사
                     </ol>
                 </ul>
